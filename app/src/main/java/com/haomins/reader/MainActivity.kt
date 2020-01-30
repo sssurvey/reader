ebb8b98241d5c5e6dfc7eb0cf91a43378917db4f
Haomin Shi <haomins1@verifone.com>
2020-01-30 00:53:37 -0500

    initial push for project

---
+++ app/src/main/java/com/haomins/reader/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.haomins.reader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
