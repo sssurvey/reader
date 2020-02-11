05045286d6b9b1b729da1e55142be86004e13149
Haomin Shi <haomins1@verifone.com>
2020-02-11 14:29:46 -0600

    remvoe useless line

--- app/src/main/java/com/haomins/reader/di/modules/AppModule.kt
+++ app/src/main/java/com/haomins/reader/di/modules/AppModule.kt
@@ -12,7 +12,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
-@Module(includes = [ViewModelModule::class])
+@Module
 class AppModule {
 
     @Singleton
