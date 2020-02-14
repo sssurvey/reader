7554c2a01fc35ca9d8f35ddf6d2dd7be12ada8df
Haomin Shi <haomins1@verifone.com>
2020-02-14 11:24:45 -0600

    added an article list recycler view

--- app/src/main/java/com/haomins/reader/view/fragments/ArticleListFragment.kt
+++ app/src/main/java/com/haomins/reader/view/fragments/ArticleListFragment.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.VisibleForTesting
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.haomins.reader.R
 import com.haomins.reader.view.activities.ArticleListActivity.Companion.SOURCE_FEED_ID
 import com.haomins.reader.viewModels.ArticleListViewModel
 import dagger.android.support.AndroidSupportInjection
+import kotlinx.android.synthetic.main.article_titile_recycler_view_item.view.*
+import kotlinx.android.synthetic.main.fragment_article_list.*
 import javax.inject.Inject
 
 class ArticleListFragment : Fragment() {
@@ -18,6 +23,15 @@ class ArticleListFragment : Fragment() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var articleListViewModel: ArticleListViewModel
+
+    private val recyclerLayoutManager by lazy {
+        LinearLayoutManager(context)
+    }
+
+    data class ArticleTitleListUiItem(
+        val title: String,
+        val postTime: String
+    )
 
     companion object {
         const val TAG = "ArticleListFragment"
@@ -36,13 +50,56 @@ class ArticleListFragment : Fragment() {
         AndroidSupportInjection.inject(this)
         articleListViewModel =
             ViewModelProviders.of(this, viewModelFactory)[ArticleListViewModel::class.java]
+        article_title_recycler_view.apply {
+            setHasFixedSize(true)
+            layoutManager = recyclerLayoutManager
+        }
+        //TODO: Delete test
+        testData()
         initiateArticleLoad(arguments)
+    }
+
+    @VisibleForTesting
+    private fun testData() {
+        val testList: MutableList<ArticleTitleListUiItem> = ArrayList()
+        for (i in 0..99) {
+            testList.add(
+                ArticleTitleListUiItem(
+                    title = "This is the test data ahah",
+                    postTime = "11:20 AM"
+                )
+            )
+        }
+        article_title_recycler_view.adapter = ArticleTitleListAdapter(testList)
     }
 
     private fun initiateArticleLoad(bundle: Bundle?) {
         bundle?.getString(SOURCE_FEED_ID)?.let {
             articleListViewModel.loadArticles(it)
         }
+    }
+
+    inner class ArticleTitleListAdapter(private val articleTitleListUiItems: List<ArticleTitleListUiItem>) :
+        RecyclerView.Adapter<ArticleTitleListAdapter.CustomViewHolder>() {
+
+        inner class CustomViewHolder(val viewHolder: View) : RecyclerView.ViewHolder(viewHolder)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+            val articleListItemView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.article_titile_recycler_view_item, parent, false)
+            return CustomViewHolder(articleListItemView)
+        }
+
+        override fun getItemCount(): Int {
+            return articleTitleListUiItems.size
+        }
+
+        override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+            holder.viewHolder.article_title_text_view.text = articleTitleListUiItems[position].title
+            holder.viewHolder.article_publish_time_text_view.text =
+                articleTitleListUiItems[position].postTime
+        }
+
     }
 
 }
