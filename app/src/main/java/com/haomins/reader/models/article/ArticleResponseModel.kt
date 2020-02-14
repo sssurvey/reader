9c000db5955605017433c2bc171fbe8ee5071e7a
haominshi <haominshi@gmail.com>
2020-02-13 20:56:31 -0600

    refactor reponse item file structure

--- app/src/main/java/com/haomins/reader/models/article/ArticleResponseModel.kt
+++ app/src/main/java/com/haomins/reader/models/article/ArticleResponseModel.kt
@@ -29,10 +29,46 @@ data class ArticleResponseModel(
     val likingUsersCount: Long
 
     /**
-     * TODO: complete this later
      * @SerializedName("annotations")
      * @SerializedName("likingUsers")
      * @SerializedName("comments")
      */
+
+)
+
+data class ArticleItemModel(
+
+    @SerializedName("crawlTimeMsec")
+    val crawlTimeMsec: String,
+
+    @SerializedName("timestampUsec")
+    val timestampUsec: String,
+
+    @SerializedName("id")
+    val id: String,
+
+    @SerializedName("categories")
+    val categories: List<String>,
+
+    @SerializedName("title")
+    val title: String,
+
+    @SerializedName("summary")
+    val summary: Summary
+
+    /**
+     * @SerializedName("published")
+     * @SerializedName("updated")
+     * @SerializedName("tor_identifier")
+     */
+)
+
+data class Summary(
+
+    @SerializedName("direction")
+    val direction: String,
+
+    @SerializedName("content")
+    val content: String
 
 )
