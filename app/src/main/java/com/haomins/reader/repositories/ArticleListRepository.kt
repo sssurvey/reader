eb57fe535af02c548750c4082920e4b8994800f3
haominshi <haominshi@gmail.com>
2020-03-29 05:21:14 -0500

    updated the way we handle refitem individule API calls

--- app/src/main/java/com/haomins/reader/repositories/ArticleListRepository.kt
+++ app/src/main/java/com/haomins/reader/repositories/ArticleListRepository.kt
@@ -1,6 +1,7 @@
 package com.haomins.reader.repositories
 
 import android.content.SharedPreferences
+import android.util.Log
 import com.haomins.reader.TheOldReaderService
 import com.haomins.reader.data.AppDatabase
 import com.haomins.reader.data.entities.ArticleEntity
@@ -9,6 +10,7 @@ import com.haomins.reader.models.article.ItemRefListResponseModel
 import com.haomins.reader.utils.getValue
 import com.haomins.reader.viewModels.LoginViewModel
 import io.reactivex.Observable
+import io.reactivex.observers.DisposableObserver
 import io.reactivex.observers.DisposableSingleObserver
 import javax.inject.Inject
 
@@ -17,6 +19,11 @@ class ArticleListRepository @Inject constructor(
     private val appDatabase: AppDatabase,
     private val sharedPreferences: SharedPreferences
 ) {
+
+    companion object {
+        const val TAG = "ArticleListRepository"
+        private const val MAX_ALLOWED_CONCURRENCY = 5
+    }
 
     private var continueId = ""
     private var isWaitingOnResponse = false
@@ -64,13 +71,13 @@ class ArticleListRepository @Inject constructor(
     }
 
     private fun loadIndividualArticleInformation(itemRefListResponseModel: ItemRefListResponseModel) {
-        itemRefListResponseModel.itemRefs.forEach {
+        Observable.fromIterable(itemRefListResponseModel.itemRefs).flatMap({
             theOldReaderService.loadArticleDetailsByRefId(
                 headerAuthValue = (TheOldReaderService.AUTH_HEADER_VALUE_PREFIX
                         + sharedPreferences.getValue(LoginViewModel.AUTH_CODE_KEY)),
                 refItemId = it.id
-            ).subscribe(ArticleResponseModelObserver())
-        }
+            ).toObservable()
+        }, MAX_ALLOWED_CONCURRENCY).subscribe(ArticleResponseModelObserver())
     }
 
     private fun saveIndividualArticleToDatabase(articleResponseModel: ArticleResponseModel) {
@@ -86,14 +93,12 @@ class ArticleListRepository @Inject constructor(
         appDatabase.articleDao().insert(articleEntity)
     }
 
-    private inner class ArticleResponseModelObserver :
-        DisposableSingleObserver<ArticleResponseModel>() {
-        override fun onSuccess(t: ArticleResponseModel) {
+    private inner class ArticleResponseModelObserver : DisposableObserver<ArticleResponseModel>() {
+        override fun onError(e: Throwable) { Log.d(TAG, "onError::${e.printStackTrace()}") }
+        override fun onComplete() { Log.d(TAG, "onComplete::called") }
+        override fun onNext(t: ArticleResponseModel) {
             saveIndividualArticleToDatabase(t)
-        }
-
-        override fun onError(e: Throwable) {
-            e.printStackTrace()
+            onComplete()
         }
     }
 
