7b663fd12fc50f60b8cbad83bc260117184e44ba
haominshi <haominshi@gmail.com>
2020-04-15 22:55:35 -0500

    added settings, and icons

--- presentation/src/main/java/com/haomins/reader/view/activities/SettingsActivity.kt
+++ presentation/src/main/java/com/haomins/reader/view/activities/SettingsActivity.kt
@@ -3,12 +3,25 @@ package com.haomins.reader.view.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.haomins.reader.R
+import com.haomins.reader.view.fragments.SettingsFragment
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        showSettingsFragment()
+    }
+
+    private fun showSettingsFragment() {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(
+                R.id.settings_container,
+                SettingsFragment(),
+                SettingsFragment.TAG
+            )
+            .commit()
     }
 
 }
