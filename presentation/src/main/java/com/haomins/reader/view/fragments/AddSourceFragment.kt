4662002f8e05dae2c3af7bf55927ec398d33de5e
haominshi <haominshi@gmail.com>
2020-04-21 16:36:40 -0500

    added ui

--- presentation/src/main/java/com/haomins/reader/view/fragments/AddSourceFragment.kt
+++ presentation/src/main/java/com/haomins/reader/view/fragments/AddSourceFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -11,6 +12,7 @@ import androidx.lifecycle.ViewModelProviders
 import com.haomins.reader.R
 import com.haomins.reader.viewModels.AddSourceViewModel
 import dagger.android.support.AndroidSupportInjection
+import kotlinx.android.synthetic.main.fragment_add_source.*
 import javax.inject.Inject
 
 class AddSourceFragment : Fragment() {
@@ -46,10 +48,33 @@ class AddSourceFragment : Fragment() {
         AndroidSupportInjection.inject(this)
         addSourceViewModel = ViewModelProviders.of(this, viewModelFactory)[AddSourceViewModel::class.java]
         registerLiveDataObservers()
+        configMediumFeedEditText()
+        setOnclickListeners()
     }
 
     private fun registerLiveDataObservers() {
         addSourceViewModel.isSourceAdded.observe(viewLifecycleOwner, isSourceAddedObserver)
     }
 
+    private fun setOnclickListeners() {
+        add_feed_button.setOnClickListener { if (!feed_input_box.text.isNullOrEmpty()) addFeedOnClick() }
+        add_medium_feed_button.setOnClickListener { if (!medium_feed_desc.text.isNullOrEmpty()) addMediumFeedOnClick() }
+    }
+
+    private fun addFeedOnClick() {
+        addSourceViewModel.addSource(source = feed_input_box.text.toString())
+    }
+
+    private fun addMediumFeedOnClick() {
+        addSourceViewModel.addMediumSource(source = medium_feed_input_box.text.toString())
+    }
+
+    private fun configMediumFeedEditText() {
+        medium_feed_input_box.doOnTextChanged { text, _, _, after ->
+            when (after > 0) {
+                true -> medium_feed_desc.text = getString(R.string.medium_feed_sample_desc_template, text)
+                false -> medium_feed_desc.text = getString(R.string.medium_feed_sample_desc)
+            }
+        }
+    }
 }
--- presentation/src/main/java/com/haomins/reader/viewModels/AddSourceViewModel.kt
+++ presentation/src/main/java/com/haomins/reader/viewModels/AddSourceViewModel.kt
@@ -1,5 +1,6 @@
 package com.haomins.reader.viewModels
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.haomins.www.core.data.models.subscription.AddSubscriptionResponseModel
@@ -12,6 +13,11 @@ class AddSourceViewModel @Inject constructor(
     private val addSourceRepository: AddSourceRepository
 ) : ViewModel() {
 
+    companion object {
+        const val TAG = "AddSourceViewModel"
+        private const val MEDIUM_RSS_FEED_BASE = "medium.com/feed/"
+    }
+
     val isSourceAdded by lazy {
         MutableLiveData(false)
     }
@@ -19,7 +25,19 @@ class AddSourceViewModel @Inject constructor(
     fun addSource(source: String) {
         addSourceRepository.addSource(source = source)
             .doOnSuccess(::checkIfSuccess)
+            .doOnError(::printError)
             .subscribe()
+    }
+
+    fun addMediumSource(source: String) {
+        addSourceRepository.addSource(source = MEDIUM_RSS_FEED_BASE + source)
+            .doOnSuccess(::checkIfSuccess)
+            .doOnError(::printError)
+            .subscribe()
+    }
+
+    private fun printError(t: Throwable) {
+        Log.d(TAG, "${t.printStackTrace()}")
     }
 
     private fun checkIfSuccess(addSubscriptionResponseModel: AddSubscriptionResponseModel) {
