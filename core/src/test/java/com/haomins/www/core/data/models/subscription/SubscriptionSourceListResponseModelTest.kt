b08e15b0b609678ecf163f8b4d976ccdef5e9724
haominshi <haominshi@gmail.com>
2020-04-29 18:41:37 -0500

    create test class for Sourcelistmodel

---
+++ core/src/test/java/com/haomins/www/core/data/models/subscription/SubscriptionSourceListResponseModelTest.kt
@@ -0,0 +1,15 @@
+package com.haomins.www.core.data.models.subscription
+
+import com.google.gson.Gson
+import org.junit.Assert.*
+import org.junit.Before
+
+class SubscriptionSourceListResponseModelTest {
+
+    private lateinit var gson: Gson
+
+    @Before
+    fun setup() {
+        gson = Gson()
+    }
+}
