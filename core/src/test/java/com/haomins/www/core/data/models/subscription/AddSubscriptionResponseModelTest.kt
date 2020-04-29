45e6b88a00f7e0fe80683b435ec6d735422fd8b3
haominshi <haominshi@gmail.com>
2020-04-29 18:06:34 -0500

    added test for addSubscriptionResonseModelTest

---
+++ core/src/test/java/com/haomins/www/core/data/models/subscription/AddSubscriptionResponseModelTest.kt
@@ -0,0 +1,24 @@
+package com.haomins.www.core.data.models.subscription
+
+import com.google.gson.Gson
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class AddSubscriptionResponseModelTest {
+
+    private lateinit var gson: Gson
+
+    @Before
+    fun setup() {
+        gson = Gson()
+    }
+
+    @Test
+    fun testParseToClass() {
+    }
+
+    @Test
+    fun testParseToJson() {
+    }
+}
