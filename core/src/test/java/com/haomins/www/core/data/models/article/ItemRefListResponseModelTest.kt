34e17c2bd6178fee660d03e0ff373fb8e654d4ba
haominshi <haominshi@gmail.com>
2020-04-29 17:36:32 -0500

    added new test

---
+++ core/src/test/java/com/haomins/www/core/data/models/article/ItemRefListResponseModelTest.kt
@@ -0,0 +1,23 @@
+package com.haomins.www.core.data.models.article
+
+import com.google.gson.Gson
+import org.junit.Before
+import org.junit.Test
+
+class ItemRefListResponseModelTest {
+
+    private lateinit var gson: Gson
+
+    @Before
+    fun setup() {
+        gson = Gson()
+    }
+
+    @Test
+    fun testParseToClass() {
+    }
+
+    @Test
+    fun testParseToJson() {
+    }
+}
