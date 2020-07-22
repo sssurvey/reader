eb6243d7f6ac4cdb78c9ee3b3068cddd1ccd8545
haominshi <haominshi@gmail.com>
2020-07-21 20:15:39 -0500

    minior change

--- core/src/main/java/com/haomins/www/core/repositories/ArticleDetailRepository.kt
+++ core/src/main/java/com/haomins/www/core/repositories/ArticleDetailRepository.kt
@@ -1,24 +1,25 @@
 package com.haomins.www.core.repositories
 
 import com.haomins.www.core.data.entities.ArticleEntity
+import com.haomins.www.core.policy.RxSchedulingPolicy
 import com.haomins.www.core.service.RoomService
 import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class ArticleDetailRepository @Inject constructor(
-    private val roomService: RoomService
+    private val roomService: RoomService,
+    private val defaultSchedulingPolicy: RxSchedulingPolicy
 ) {
 
     fun loadArticleDetail(itemId: String): Single<ArticleEntity> {
-        return roomService
-            .articleDao()
-            .selectArticleByItemId(itemId)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
+        with(defaultSchedulingPolicy) {
+            return roomService
+                .articleDao()
+                .selectArticleByItemId(itemId)
+                .defaultSchedulingPolicy()
+        }
     }
 
 }
