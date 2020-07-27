8ec560c4f50574eba643ed5aa136741f8f7d4223
haominshi <haominshi@gmail.com>
2020-07-26 21:28:19 -0500

    fix final class mock issue

---
+++ core/src/test/java/com/haomins/www/core/repositories/SourceSubscriptionListRepositoryTest.kt
@@ -0,0 +1,34 @@
+package com.haomins.www.core.repositories
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SourceSubscriptionListRepositoryTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun loadSubList() {
+    }
+
+    @Test
+    fun loadIconImage() {
+    }
+
+    @Test
+    fun retrieveSubListFromDB() {
+    }
+
+    @Test
+    fun saveSubListToDB() {
+    }
+}
