4b61e56442d5d6221fef8ae4e72c1db3f657087d
haominshi <haominshi@gmail.com>
2020-08-10 20:23:23 -0500

    working test for 1 method of ArticleRepository

--- core/src/test/java/com/haomins/www/core/repositories/ArticleListRepositoryTest.kt
+++ core/src/test/java/com/haomins/www/core/repositories/ArticleListRepositoryTest.kt
@@ -1,29 +1,40 @@
 package com.haomins.www.core.repositories
 
 import android.content.SharedPreferences
+import com.haomins.www.core.MockTheOldReaderService
 import com.haomins.www.core.TestSchedulingStrategy
+import com.haomins.www.core.data.SharedPreferenceKey
+import com.haomins.www.core.data.entities.ArticleEntity
+import com.haomins.www.core.db.dao.ArticleDao
 import com.haomins.www.core.service.RoomService
-import com.haomins.www.core.service.TheOldReaderService
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.verify
+import io.reactivex.Observable
+import io.reactivex.observers.TestObserver
 import io.reactivex.schedulers.TestScheduler
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.times
 import org.mockito.MockitoAnnotations
+import java.util.concurrent.TimeUnit
 
 class ArticleListRepositoryTest {
 
     @Mock
-    lateinit var mockTheOldReaderService: TheOldReaderService
+    lateinit var mockRoomService: RoomService
 
     @Mock
-    lateinit var mockRoomService: RoomService
+    lateinit var mockArticleDao: ArticleDao
 
     @Mock
     lateinit var mockSharedPreferences: SharedPreferences
 
     private val testScheduler = TestScheduler()
     private val testSchedulingStrategy = TestSchedulingStrategy(subscribeOnScheduler = testScheduler)
+    private val mockTheOldReaderService = MockTheOldReaderService(testSchedulingStrategy)
     private lateinit var articleListRepository: ArticleListRepository
 
     @Before
@@ -35,6 +46,7 @@ class ArticleListRepositoryTest {
             mockSharedPreferences,
             testSchedulingStrategy
         )
+        mockHelper()
     }
 
     @After
@@ -43,6 +55,14 @@ class ArticleListRepositoryTest {
 
     @Test
     fun loadAllArticleItemRefs() {
+        val testObserver = TestObserver<List<ArticleEntity>>()
+        articleListRepository.loadAllArticleItemRefs().subscribe(testObserver)
+        testObserver.assertSubscribed()
+        testScheduler.advanceTimeBy(2000, TimeUnit.MILLISECONDS)
+        verify(mockRoomService, times(20)).articleDao()
+        verify(mockArticleDao, times(10)).insert(any())
+        testObserver.assertValueCount(1)
+        testObserver.assertComplete()
     }
 
     @Test
@@ -58,6 +78,44 @@ class ArticleListRepositoryTest {
     }
 
     private fun mockHelper() {
-        
+        `when`(mockSharedPreferences
+            .getString(SharedPreferenceKey.AUTH_CODE_KEY.string, ""))
+            .thenReturn("test_auth_code")
+        `when`(mockRoomService.articleDao())
+            .thenReturn(mockArticleDao)
+        `when`(mockArticleDao.getAll())
+            .thenReturn(
+                        Observable.just(
+                            mutableListOf(
+                                ArticleEntity(
+                                    "1",
+                                    "2",
+                                    "test_title",
+                                    1,
+                                    1,
+                                    "test_author",
+                                    "test_content"
+                                ),
+                                ArticleEntity(
+                                    "2",
+                                    "2",
+                                    "test_title",
+                                    1,
+                                    1,
+                                    "test_author",
+                                    "test_content"
+                                ),
+                                ArticleEntity(
+                                    "3",
+                                    "2",
+                                    "test_title",
+                                    1,
+                                    1,
+                                    "test_author",
+                                    "test_content"
+                                )
+                            )
+                        )
+            )
     }
 }
