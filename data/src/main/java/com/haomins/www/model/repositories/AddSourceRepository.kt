79b1cf76a33b3be4818c0f2ed370818509623377
haominshi <haominshi@gmail.com>
2021-04-19 20:08:23 -0400

    complete: added unit test for add source repository

--- data/src/main/java/com/haomins/www/model/repositories/AddSourceRepository.kt
+++ data/src/main/java/com/haomins/www/model/repositories/AddSourceRepository.kt
@@ -6,28 +6,26 @@ import com.haomins.domain.repositories.AddSourceRepositoryContract
 import com.haomins.www.model.mapper.AddSourceResponseModelMapper
 import com.haomins.www.model.model.SharedPreferenceKey
 import com.haomins.www.model.service.TheOldReaderService
-import com.haomins.www.model.strategies.RxSchedulingStrategy
 import com.haomins.www.model.util.getString
 import io.reactivex.Single
 import javax.inject.Inject
 
 class AddSourceRepository @Inject constructor(
-        private val addSourceResponseModelMapper: AddSourceResponseModelMapper,
-        private val theOldReaderService: TheOldReaderService,
-        private val sharedPreferences: SharedPreferences,
-        private val defaultSchedulingStrategy: RxSchedulingStrategy
+    private val addSourceResponseModelMapper: AddSourceResponseModelMapper,
+    private val theOldReaderService: TheOldReaderService,
+    private val sharedPreferences: SharedPreferences
 ) : AddSourceRepositoryContract {
 
     override fun addSource(source: String): Single<AddSourceResponseModel> {
-        with(defaultSchedulingStrategy) {
-            return theOldReaderService.addSubscription(
-                    headerAuthValue = (TheOldReaderService.AUTH_HEADER_VALUE_PREFIX
-                            + sharedPreferences.getString(SharedPreferenceKey.AUTH_CODE_KEY)),
-                    quickAddSubscription = source
+        return theOldReaderService
+            .addSubscription(
+                headerAuthValue = (TheOldReaderService.AUTH_HEADER_VALUE_PREFIX
+                        + sharedPreferences.getString(SharedPreferenceKey.AUTH_CODE_KEY)),
+                quickAddSubscription = source
             )
-                    .map { addSourceResponseModelMapper.dataModelToDomainModel(it) }
-                    .useDefaultSchedulingPolicy()
-        }
+            .map {
+                addSourceResponseModelMapper.dataModelToDomainModel(it)
+            }
     }
 
 }
