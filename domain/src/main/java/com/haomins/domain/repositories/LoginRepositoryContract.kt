71612c15438f751d546bb482328c3cfee78df917
haominshi <haominshi@gmail.com>
2021-05-04 14:30:45 -0400

    edited: completed repo contract, and mapper

--- domain/src/main/java/com/haomins/domain/repositories/LoginRepositoryContract.kt
+++ domain/src/main/java/com/haomins/domain/repositories/LoginRepositoryContract.kt
@@ -1,10 +1,11 @@
 package com.haomins.domain.repositories
 
+import com.haomins.domain.model.UserAuthResponseModel
 import io.reactivex.Single
 
 interface LoginRepositoryContract {
 
-    fun login(user: Pair<String, String>): Single<Any>
+    fun login(user: Pair<String, String>): Single<UserAuthResponseModel>
 
     fun getSignUpUrlString(): String
 
