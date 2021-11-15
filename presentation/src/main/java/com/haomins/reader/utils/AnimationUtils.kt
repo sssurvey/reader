cc2137dfa5e625e2aaacf7e26d8b20807f7d44e2
haominshi <haominshi@gmail.com>
2021-11-14 21:35:34 -0500

    #102: added AnimationUtil.delayedUiOperation(args)

--- presentation/src/main/java/com/haomins/reader/utils/AnimationUtils.kt
+++ presentation/src/main/java/com/haomins/reader/utils/AnimationUtils.kt
@@ -1,7 +1,16 @@
 package com.haomins.reader.utils
 
 import android.app.Activity
+import android.util.Log
 import com.haomins.reader.R
+import io.reactivex.Scheduler
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+private const val TAG = "AnimationUtils"
 
 fun Activity.slideInAnimation() {
     this.overridePendingTransition(R.anim.start_slide_in_left, R.anim.start_slide_out_left)
@@ -9,4 +18,25 @@ fun Activity.slideInAnimation() {
 
 fun Activity.slideOutAnimation() {
     this.overridePendingTransition(R.anim.start_slide_in_right, R.anim.start_slide_out_right)
+}
+
+fun Activity.delayedUiOperation(
+    seconds: Long,
+    doAfterDelay: () -> Unit,
+    doOnError: (Throwable) -> Unit = { Log.e(TAG, "onError :: ${it.printStackTrace()}") },
+    executionScheduler: Scheduler = Schedulers.io(),
+    observationScheduler: Scheduler = AndroidSchedulers.mainThread()
+) {
+    return Single
+        .timer(seconds, TimeUnit.SECONDS, executionScheduler)
+        .observeOn(observationScheduler)
+        .subscribe(object : DisposableSingleObserver<Long>() {
+            override fun onSuccess(t: Long) {
+                doAfterDelay.invoke()
+            }
+
+            override fun onError(e: Throwable) {
+                doOnError.invoke(e)
+            }
+        })
 }
