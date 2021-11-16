f91006bdd41502831e094d23ae3449bec9e469d5
haominshi <haominshi@gmail.com>
2021-11-15 21:02:04 -0500

    created a demo project module for testing UI module

---
+++ ui/src/main/java/com/haomins/ui/edittext/ReaderEditText.kt
@@ -0,0 +1,26 @@
+package com.haomins.ui.edittext
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+
+class ReaderEditText : AppCompatEditText {
+
+    constructor(context: Context) : super(context) {
+        AppCompatEditText(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+
+    }
+
+
+}
