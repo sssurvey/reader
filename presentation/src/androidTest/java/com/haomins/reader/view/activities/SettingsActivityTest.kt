4ca60f15d9ac2650173af4819472b862c397123d
Haomin Shi <haominshi@Haomins-MacBook-Pro.local>
2022-06-17 19:24:56 -0400

    added: empty test & dependecies

---
+++ presentation/src/androidTest/java/com/haomins/reader/view/activities/SettingsActivityTest.kt
@@ -0,0 +1,28 @@
+package com.haomins.reader.view.activities
+
+internal class SettingsActivityTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    fun tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun onCreate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun onBackPressed() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun showAboutFragment() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun showDisclosureFragment() {
+    }
+}
