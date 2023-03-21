c2ff9bd0f57ef5e079fac803a3234d223adbef4f
Haomin Shi <haominshi@gmail.com>
2023-03-21 17:18:52 -0400

    save work: added a new local ds for local sources

--- data/src/main/java/com/haomins/data/db/dao/SubscriptionDao.kt
+++ data/src/main/java/com/haomins/data/db/dao/SubscriptionDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.haomins.data_model.entity.SubscriptionEntity
+import io.reactivex.Completable
 import io.reactivex.Single
 
 @Dao
@@ -18,5 +19,11 @@ interface SubscriptionDao {
 
     @Query("SELECT * FROM subscription_entity")
     fun getAll(): Single<List<SubscriptionEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAllV2(sources: List<SubscriptionEntity>): Completable
+
+    @Query("SELECT * FROM subscription_entity")
+    fun getAllV2(): Single<List<SubscriptionEntity>>
 
 }
---
+++ data/src/main/java/com/haomins/data/repositories/local/SourceLocalDataStore.kt
@@ -0,0 +1,23 @@
+package com.haomins.data.repositories.local
+
+import com.haomins.data.db.dao.SubscriptionDao
+import com.haomins.data_model.entity.SubscriptionEntity
+import io.reactivex.Completable
+import io.reactivex.Single
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SourceLocalDataStore @Inject constructor(
+    private val subscriptionDao: SubscriptionDao
+) {
+
+    fun saveAllSourcesToLocal(sources: List<SubscriptionEntity>): Completable {
+        return subscriptionDao.insertAllV2(sources)
+    }
+
+    fun loadAllSourcesFromLocal(): Single<List<SubscriptionEntity>> {
+        return subscriptionDao.getAllV2()
+    }
+
+}
