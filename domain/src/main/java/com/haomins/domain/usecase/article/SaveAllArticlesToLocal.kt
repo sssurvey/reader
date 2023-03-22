93b5c4bebdbd43a566828d69065d2735ab746256
Haomin Shi <haominshi@gmail.com>
2023-03-22 17:08:37 -0400

    added: SaveAllArticlesToLocal

---
+++ domain/src/main/java/com/haomins/domain/usecase/article/SaveAllArticlesToLocal.kt
@@ -0,0 +1,40 @@
+package com.haomins.domain.usecase.article
+
+import com.haomins.domain.exception.ParamsShouldNotBeNullException
+import com.haomins.domain.repositories.local.ArticleListLocalRepository
+import com.haomins.domain.scheduler.ExecutionScheduler
+import com.haomins.domain.scheduler.PostExecutionScheduler
+import com.haomins.domain.usecase.CompletableUseCase
+import com.haomins.model.entity.ArticleEntity
+import io.reactivex.Completable
+import javax.inject.Inject
+
+class SaveAllArticlesToLocal @Inject constructor(
+    private val articleListLocalRepository: ArticleListLocalRepository,
+    executionScheduler: ExecutionScheduler,
+    postExecutionScheduler: PostExecutionScheduler
+) : CompletableUseCase<SaveAllArticlesToLocal.Companion.Params>(
+    executionScheduler,
+    postExecutionScheduler
+) {
+
+    override fun buildUseCaseCompletable(params: Params?): Completable {
+        if (params == null) throw ParamsShouldNotBeNullException()
+        return articleListLocalRepository
+            .saveAllArticles(
+                params.articleEntities
+            )
+    }
+
+    companion object {
+
+        data class Params(
+            val articleEntities: List<ArticleEntity>
+        )
+
+        fun forSaveAllArticlesToLocal(articleEntities: List<ArticleEntity>): Params {
+            return Params(articleEntities)
+        }
+    }
+
+}
