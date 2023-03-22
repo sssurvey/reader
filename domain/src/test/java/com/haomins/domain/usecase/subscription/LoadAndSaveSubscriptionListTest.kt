1e62215eb4201e006ab33ec6b0f5d432364e1361
Haomin Shi <haominshi@gmail.com>
2023-03-22 15:31:10 -0400

    unittest: LoadAndSaveSubscriptionList

--- domain/src/test/java/com/haomins/domain/usecase/subscription/LoadAndSaveSubscriptionListTest.kt
+++ domain/src/test/java/com/haomins/domain/usecase/subscription/LoadAndSaveSubscriptionListTest.kt
@@ -1,13 +1,18 @@
 package com.haomins.domain.usecase.subscription
 
+import com.haomins.domain.MockGenerator
 import com.haomins.domain.TestSchedulers
-import org.junit.Assert.*
-
+import io.reactivex.Completable
+import io.reactivex.Single
 import org.junit.After
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
 
 class LoadAndSaveSubscriptionListTest {
 
@@ -45,6 +50,88 @@ class LoadAndSaveSubscriptionListTest {
 
     @Test
     fun `are correct instance called when buildUseCaseSuccessful`() {
-        
+        val mockSubscriptionItemList = MockGenerator.generateSubscriptionListResponseModel().subscriptions
+        val mockSubscriptionEntityList = loadAndSaveSubscriptionList.convertSubscriptionItemModelToEntityForTesting(mockSubscriptionItemList)
+        `when`(
+            loadSubscriptionListFromRemote.buildUseCaseSingle(Unit)
+        )
+            .then {
+                Single.fromCallable { mockSubscriptionItemList }
+            }
+        `when`(
+            saveSubscriptionListToLocal.buildUseCaseCompletable(
+                SaveSubscriptionListToLocal.forSaveSubscriptionList(
+                    mockSubscriptionEntityList
+                )
+            )
+        )
+            .then { Completable.complete() }
+        `when`(loadSubscriptionListFromLocal.buildUseCaseSingle(Unit))
+            .then {
+                Single.fromCallable { mockSubscriptionEntityList }
+            }
+
+        val testObserver = loadAndSaveSubscriptionList.buildUseCaseSingle(Unit).test()
+
+        testObserver.assertComplete()
+
+        verify(loadSubscriptionListFromRemote, times(1)).buildUseCaseSingle(Unit)
+        verify(saveSubscriptionListToLocal, times(1)).buildUseCaseCompletable(
+            SaveSubscriptionListToLocal.forSaveSubscriptionList(mockSubscriptionEntityList)
+        )
+        verify(loadSubscriptionListFromLocal, times(1)).buildUseCaseSingle(Unit)
+    }
+
+    @Test
+    fun `are correct instance called when buildUseCase OnError`() {
+
+        val mockSubscriptionItemList = MockGenerator.generateSubscriptionListResponseModel().subscriptions
+        val mockSubscriptionEntityList = loadAndSaveSubscriptionList.convertSubscriptionItemModelToEntityForTesting(mockSubscriptionItemList)
+
+        `when`(
+            loadSubscriptionListFromRemote.buildUseCaseSingle(Unit)
+        )
+            .then {
+                Single.fromCallable {
+                    throw Exception("")
+                }
+            }
+
+        `when`(
+            saveSubscriptionListToLocal.buildUseCaseCompletable(
+                SaveSubscriptionListToLocal.forSaveSubscriptionList(
+                    mockSubscriptionEntityList
+                )
+            )
+        )
+            .then { Completable.error(Throwable("")) }
+
+        `when`(loadSubscriptionListFromLocal.buildUseCaseSingle(Unit))
+            .then {
+                Single.fromCallable { mockSubscriptionEntityList }
+            }
+
+        val testObserver = loadAndSaveSubscriptionList.buildUseCaseSingle(Unit).test()
+
+        testObserver.assertComplete()
+
+        verify(loadSubscriptionListFromRemote, times(1)).buildUseCaseSingle(Unit)
+        verify(saveSubscriptionListToLocal, times(0)).buildUseCaseCompletable(
+            SaveSubscriptionListToLocal.forSaveSubscriptionList(mockSubscriptionEntityList)
+        )
+        // Normal for loadSubscriptionListFromLocal#buildUseCaseSingle to be called twice,
+        // https://stackoverflow.com/questions/56516502/rxjava2-andthen-called-even-though-first-completable-emitted-error
+        verify(loadSubscriptionListFromLocal, times(2)).buildUseCaseSingle(Unit)
+    }
+
+    @Test
+    fun `is convertSubscriptionItemModelToEntityForTesting correct`() {
+
+        val mockSubscriptionItemList = MockGenerator.generateSubscriptionListResponseModel().subscriptions
+        val mockSubscriptionEntityList = loadAndSaveSubscriptionList.convertSubscriptionItemModelToEntityForTesting(mockSubscriptionItemList)
+
+        for (i in 0 until mockSubscriptionItemList.size) {
+            assertTrue(mockSubscriptionItemList[i].id == mockSubscriptionEntityList[i].id)
+        }
     }
 }
