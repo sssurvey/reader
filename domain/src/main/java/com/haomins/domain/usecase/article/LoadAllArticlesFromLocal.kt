7752add3f4023116f7806f64a36f2b2ecb5deb7e
Haomin Shi <haominshi@gmail.com>
2023-03-22 17:15:23 -0400

    added: LoadAllArticlesFromLocal

---
+++ domain/src/main/java/com/haomins/domain/usecase/article/LoadAllArticlesFromLocal.kt
@@ -0,0 +1,25 @@
+package com.haomins.domain.usecase.article
+
+import com.haomins.domain.repositories.local.ArticleListLocalRepository
+import com.haomins.domain.scheduler.ExecutionScheduler
+import com.haomins.domain.scheduler.PostExecutionScheduler
+import com.haomins.domain.usecase.SingleUseCase
+import com.haomins.model.entity.ArticleEntity
+import io.reactivex.Single
+import javax.inject.Inject
+
+class LoadAllArticlesFromLocal @Inject constructor(
+    private val articleListLocalRepository: ArticleListLocalRepository,
+    executionScheduler: ExecutionScheduler,
+    postExecutionScheduler: PostExecutionScheduler
+) : SingleUseCase<Unit, List<ArticleEntity>>(
+    executionScheduler = executionScheduler,
+    postExecutionScheduler = postExecutionScheduler
+) {
+
+    override fun buildUseCaseSingle(params: Unit?): Single<List<ArticleEntity>> {
+        return articleListLocalRepository
+            .loadAllArticles()
+    }
+
+}
