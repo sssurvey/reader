22ae2f4a4276d163b10c5f5a7174a4170a66fc94
Haomin Shi <haominshi@gmail.com>
2023-03-28 15:37:27 -0400

    comment: added comment for article list remote repo

--- domain/src/main/java/com/haomins/domain/repositories/remote/ArticleListRemoteRepository.kt
+++ domain/src/main/java/com/haomins/domain/repositories/remote/ArticleListRemoteRepository.kt
@@ -5,8 +5,34 @@ import io.reactivex.Single
 
 interface ArticleListRemoteRepository {
 
+    /**
+     * Load article items from remote source.
+     *
+     * Since the implementation of paging from remote is based on the [continueId], therefore
+     * if [loadAllArticleItemsFromRemote] is called with empty [continueId], then first page is of
+     * items are requested, the return [Single] will contain a list of [Pair] where [Pair.first] is
+     * the [continueId] we can use to call the next page, and [Pair.second] is the content of article.
+     *
+     * @param continueId Empty string for getting the first page, new [continueId] will be returned
+     * via each request.
+     *
+     * @return [Single] that contains a [List] of [Pair], with [Pair.first] being the [continueId]
+     * for next page, and [Pair.second] as the content of the [ArticleResponseModel].
+     */
     fun loadAllArticleItemsFromRemote(continueId: String): Single<List<Pair<String, ArticleResponseModel>>>
 
+    /**
+     * Load article items from remote source limited by feed.
+     *
+     * @see loadAllArticleItemsFromRemote the idea of these two API are similar.
+     *
+     * @param feedId string so that we can request article from specific feed source.
+     * @param continueId Empty string for getting the first page, new [continueId] will be returned
+     * via each request.
+     *
+     * @return [Single] that contains a [List] of [Pair], with [Pair.first] being the [continueId]
+     * for next page, and [Pair.second] as the content of the [ArticleResponseModel].
+     */
     fun loadAllArticleItemsFromRemoteWithFeed(
         feedId: String,
         continueId: String
