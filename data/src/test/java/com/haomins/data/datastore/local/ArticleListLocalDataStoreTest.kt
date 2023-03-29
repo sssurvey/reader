9c4ede72745b7ea411c252a0bcc4d0eb335fc1b1
Haomin Shi <haominshi@gmail.com>
2023-03-29 13:10:07 -0400

    unittest: articleListRemoteDataStore

---
+++ data/src/test/java/com/haomins/data/datastore/local/ArticleListLocalDataStoreTest.kt
@@ -0,0 +1,30 @@
+package com.haomins.data.datastore.local
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class ArticleListLocalDataStoreTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun saveAllArticles() {
+    }
+
+    @Test
+    fun loadAllArticles() {
+    }
+
+    @Test
+    fun loadAllArticlesFromFeed() {
+    }
+}
--- data/src/test/java/com/haomins/data/datastore/remote/ArticleListRemoteDataStoreTest.kt
+++ data/src/test/java/com/haomins/data/datastore/remote/ArticleListRemoteDataStoreTest.kt
@@ -2,144 +2,81 @@ package com.haomins.data.datastore.remote
 
 import android.content.SharedPreferences
 import com.haomins.data.MockTheOldReaderService
-import com.haomins.data.service.TheOldReaderService
 import com.haomins.model.SharedPreferenceKey
 import com.haomins.model.remote.article.ArticleResponseModel
-import io.reactivex.Single
 import io.reactivex.observers.TestObserver
 import org.junit.After
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
-import org.mockito.Mockito.*
+import org.mockito.Mockito.times
+import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 import org.mockito.Spy
 import org.mockito.kotlin.any
 import org.mockito.kotlin.verify
 
-//class ArticleListRemoteDataStoreTest {
-//
-//    private lateinit var articleListRemoteDataStore: ArticleListRemoteDataStore
-//
-//    @Spy
-//    val mockTheOldReaderService = MockTheOldReaderService()
-//
-//    @Mock
-//    lateinit var mockSharedPreferences: SharedPreferences
-//
-//    @Before
-//    fun setUp() {
-//        MockitoAnnotations.openMocks(this)
-//        articleListRemoteDataStore = ArticleListRemoteDataStore(
-//            mockTheOldReaderService,
-//            mockSharedPreferences,
-//        )
-//        mockHelper()
-//    }
-//
-//    @After
-//    fun tearDown() {
-//    }
-//
-//    @Test
-//    fun `test loadAllArticleItems() order verification`() {
-//        val testObserver = TestObserver<List<ArticleResponseModel>>()
-//        articleListRemoteDataStore.loadAllArticleItems().subscribe(testObserver)
-//        testObserver.assertSubscribed()
-//        verify(mockTheOldReaderService, times(1))
-//            .loadAllArticles(any(), any(), any(), any(), any())
-//        verify(mockTheOldReaderService, times(10))
-//            .loadArticleDetailsByRefId(any(), any(), any())
-//        testObserver.assertValueCount(1)
-//        testObserver.assertComplete()
-//    }
-//
-//    @Test
-//    fun `test loadArticleItemRefs() order verification`() {
-//        val testObserver = TestObserver<List<ArticleResponseModel>>()
-//        articleListRemoteDataStore.loadArticleAllItems("test_feed_id").subscribe(testObserver)
-//        testObserver.assertSubscribed()
-//        verify(mockTheOldReaderService)
-//            .loadArticleListByFeed(any(), any(), any(), any(), any())
-//        verify(mockTheOldReaderService, times(10))
-//            .loadArticleDetailsByRefId(any(), any(), any())
-//    }
-//
-//    @Test
-//    fun `test should load articles from DB directly by feed if error was thrown during api access`() {
-//        val noConnectionMockTheOldReaderService = mock(TheOldReaderService::class.java)
-//        val testObserver = TestObserver<List<ArticleResponseModel>>()
-//        val noConnectionArticleListRemoteDataStore = ArticleListRemoteDataStore(
-//            noConnectionMockTheOldReaderService,
-//            mockSharedPreferences
-//        )
-//        `when`(
-//            noConnectionMockTheOldReaderService.loadArticleListByFeed(
-//                any(),
-//                any(),
-//                any(),
-//                any(),
-//                any()
-//            )
-//        ).thenReturn(
-//            Single.error { Exception("test_exception") }
-//        )
-//        noConnectionArticleListRemoteDataStore.loadArticleAllItems("test_feed_id")
-//            .subscribe(testObserver)
-//        testObserver.assertSubscribed()
-//    }
-//
-//    @Test
-//    fun `test should load all articles from DB directly if error was thrown during api access`() {
-//        val noConnectionMockTheOldReaderService = mock(TheOldReaderService::class.java)
-//        val testObserver = TestObserver<List<ArticleResponseModel>>()
-//        val noConnectionArticleListRemoteDataStore = ArticleListRemoteDataStore(
-//            noConnectionMockTheOldReaderService,
-//            mockSharedPreferences
-//        )
-//        `when`(
-//            noConnectionMockTheOldReaderService.loadAllArticles(
-//                any(),
-//                any(),
-//                any(),
-//                any(),
-//                any()
-//            )
-//        ).thenReturn(
-//            Single.error { Exception("test_exception") }
-//        )
-//        noConnectionArticleListRemoteDataStore.loadAllArticleItems().subscribe(testObserver)
-//        testObserver.assertSubscribed()
-//    }
-//
-//    @Test
-//    fun `test continueLoadAllArticleItems()`() {
-//        val testObserver = TestObserver<List<ArticleResponseModel>>()
-//        articleListRemoteDataStore.continueLoadAllArticleItems().subscribe(testObserver)
-//        testObserver.assertSubscribed()
-//        verify(mockTheOldReaderService)
-//            .loadAllArticles(any(), any(), any(), any(), any())
-//        verify(mockTheOldReaderService, times(10))
-//            .loadArticleDetailsByRefId(any(), any(), any())
-//    }
-//
-//    @Test
-//    fun continueLoadArticleItemRefs() {
-//        val testObserver = TestObserver<List<ArticleResponseModel>>()
-//        articleListRemoteDataStore.loadAllArticleItemsFromRemoteWithFeed("test_feed_id")
-//            .subscribe(testObserver)
-//        testObserver.assertSubscribed()
-//        verify(mockTheOldReaderService)
-//            .loadArticleListByFeed(any(), any(), any(), any(), any())
-//        verify(mockTheOldReaderService, times(10))
-//            .loadArticleDetailsByRefId(any(), any(), any())
-//    }
-//
-//    private fun mockHelper() {
-//        `when`(
-//            mockSharedPreferences
-//                .getString(SharedPreferenceKey.AUTH_CODE_KEY.string, "")
-//        )
-//            .thenReturn("test_auth_code")
-//    }
-//}
+class ArticleListRemoteDataStoreTest {
+
+    private lateinit var articleListRemoteDataStore: ArticleListRemoteDataStore
+
+    @Spy
+    val mockTheOldReaderService = MockTheOldReaderService()
+
+    @Mock
+    lateinit var mockSharedPreferences: SharedPreferences
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+        articleListRemoteDataStore = ArticleListRemoteDataStore(
+            mockTheOldReaderService,
+            mockSharedPreferences,
+        )
+        mockHelper()
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun `test loadAllArticleItemsFromRemote() should invoke correct classes`() {
+        val testObserver = TestObserver<Pair<String, List<ArticleResponseModel>>>()
+        articleListRemoteDataStore.loadAllArticleItemsFromRemote(
+            ""
+        ).subscribe(testObserver)
+        testObserver.assertSubscribed()
+        verify(mockTheOldReaderService, times(1))
+            .loadAllArticles(any(), any(), any(), any(), any())
+        verify(mockTheOldReaderService, times(10))
+            .loadArticleDetailsByRefId(any(), any(), any())
+        testObserver.assertValueCount(1)
+        testObserver.assertComplete()
+        assertTrue(testObserver.values().first().first == "test_continuation")
+    }
+
+    @Test
+    fun `test loadAllArticleItemsFromRemoteWithFeed() should invoke correct classes`() {
+        val testObserver = TestObserver<Pair<String, List<ArticleResponseModel>>>()
+        articleListRemoteDataStore.loadAllArticleItemsFromRemoteWithFeed(
+            "test_feed_id",
+            ""
+        ).subscribe(testObserver)
+        testObserver.assertSubscribed()
+        verify(mockTheOldReaderService)
+            .loadArticleListByFeed(any(), any(), any(), any(), any())
+        verify(mockTheOldReaderService, times(10))
+            .loadArticleDetailsByRefId(any(), any(), any())
+        assertTrue(testObserver.values().first().first == "test_continuation")
+    }
+
+    private fun mockHelper() {
+        `when`(
+            mockSharedPreferences
+                .getString(SharedPreferenceKey.AUTH_CODE_KEY.string, "")
+        )
+            .thenReturn("test_auth_code")
+    }
+}
