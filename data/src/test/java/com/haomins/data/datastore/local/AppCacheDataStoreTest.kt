f5fdffd4c16662710365736249fd96b97be96b1e
Haomin Shi <haominshi@gmail.com>
2023-05-10 16:02:50 -0400

    added: unit test for app cache data store

--- data/src/test/java/com/haomins/data/datastore/local/AppCacheDataStoreTest.kt
+++ data/src/test/java/com/haomins/data/datastore/local/AppCacheDataStoreTest.kt
@@ -1,22 +1,88 @@
 package com.haomins.data.datastore.local
 
-import org.junit.Assert.*
-
+import com.haomins.data.util.ContextUtils
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.times
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.verify
+import java.io.File
 
-//TODO: 
 class AppCacheDataStoreTest {
+
+    @Mock
+    lateinit var mockContextUtils: ContextUtils
+
+    @Mock
+    lateinit var cacheData: File
+
+    @Mock
+    lateinit var validFileDir: File
+
+    @Mock
+    lateinit var validFileItem: File
+
+    @Mock
+    lateinit var dummyFile: File
+
+    private lateinit var appCacheDataStore: AppCacheDataStore
 
     @Before
     fun setUp() {
+
+        MockitoAnnotations.openMocks(this)
+        `when`(dummyFile.length()).thenReturn(2048 * 1024)
+        `when`(dummyFile.isDirectory).thenReturn(false)
+        `when`(validFileItem.length()).thenReturn(4096 * 1024)
+        `when`(validFileDir.isDirectory).thenReturn(true)
+        `when`(validFileDir.listFiles()).thenReturn(arrayOf(dummyFile, dummyFile))
+        `when`(validFileItem.isDirectory).thenReturn(false)
+        `when`(validFileItem.listFiles()).thenReturn(arrayOf(dummyFile))
+
+        val mockFileList = arrayListOf<File>().apply {
+            (0..10).forEach {
+                add(
+                    if (it % 2 != 0) {
+                        validFileDir
+                    } else {
+                        validFileItem
+                    }
+                )
+            }
+        }
+
+
+        `when`(cacheData.listFiles()).thenReturn(mockFileList.toTypedArray())
+
+        appCacheDataStore = AppCacheDataStore(
+            mockContextUtils
+        )
     }
 
     @Test
-    fun getCurrentCacheSize() {
+    fun `is AppCacheDataStore initialized correctly`() {
+        assert(this::appCacheDataStore.isInitialized)
+    }
+
+    @Test
+    fun `getCurrentCacheSize should return file size in MB`() {
+        `when`(mockContextUtils.getCacheDir()).thenReturn(
+            cacheData
+        )
+        val testObserver = appCacheDataStore.getCurrentCacheSize().test()
+        testObserver.assertComplete()
+        assert(testObserver.values().first() == 44L)
     }
 
     @Test
     fun clearCache() {
+        `when`(mockContextUtils.getCacheDir()).thenReturn(
+            cacheData
+        )
+        val testObserver = appCacheDataStore.clearCache().test()
+        testObserver.assertComplete()
+        verify(dummyFile, times(10)).delete()
     }
 }
